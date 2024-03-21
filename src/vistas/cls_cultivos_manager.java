fcc88329c534cf1fa20f8536990b3c7ce5006637
Yeison Cordoba <corba.ye99@gmail.com>
2024-03-20 23:06:02 -0500

    Creando clase cls_cultivos_manager

---
+++ src/vistas/cls_cultivos_manager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vistas;
+
+import java.util.LinkedList;
+import javax.swing.JOptionPane;
+
+public class cls_cultivos_manager {
+    protected LinkedList<cls_cultivos> cultivos = new LinkedList<>();
+    protected boolean sw;
+    protected boolean boton_actualizar = false;
+    protected boolean id_actualizar = true;
+    protected int pos;
+    protected String codigo;
+    protected String nombre;
+    protected String area;
+}
