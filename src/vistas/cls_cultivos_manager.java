7bfd4a84bf522709a30fb2886f8c813eb84d6d7a
Yeison Cordoba <corba.ye99@gmail.com>
2024-03-20 23:12:10 -0500

    Función guardar cultivos

--- src/vistas/cls_cultivos_manager.java
+++ src/vistas/cls_cultivos_manager.java
@@ -16,4 +16,28 @@ public class cls_cultivos_manager {
     protected String codigo;
     protected String nombre;
     protected String area;
+    
+    private void fnt_sub_consulta(String codigo){
+        for(int i = 0; i < cultivos.size(); i++){
+            if (cultivos.get(i).getCodigo_str().equals(codigo)) {
+                sw = true;
+                pos = i;
+            }
+        }
+    }
+    public void fnt_guardar_cultivo(String codigo_str, String nombre_str, String area_str){
+        sw = false;
+        if (!codigo_str.equals("") && !nombre_str.equals("") && !area_str.equals("")) {
+            fnt_sub_consulta(codigo_str);
+            if (!sw) {
+                cultivos.add(new cls_cultivos(codigo_str, nombre_str, area_str));
+                JOptionPane.showMessageDialog(null, "Cultivo registrado éxitosamente","CULTIVOS",JOptionPane.INFORMATION_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(null,"Se encontro un registro existente con el mismo CÓDIGO","CULTIVOS",JOptionPane.WARNING_MESSAGE);
+            } 
+        }else{
+            JOptionPane.showMessageDialog(null,"Debe rellenar todos los datos solicitados","CULTIVOS",JOptionPane.WARNING_MESSAGE);
+        } 
+    }
+
 }
